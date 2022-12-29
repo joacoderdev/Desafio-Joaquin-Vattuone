c1686c9f4476832087e48c8f5060b8a82730fb1e
Joacoder <joacoder.dev@gmail.com>
2022-12-28 23:17:41 -0300

    ProyectoFinal1_0

---
+++ ProyectoFinal1_0/src/classes/FileManager.js
@@ -0,0 +1,117 @@
+import fs from "fs";
+
+class FileManager {
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
+
+  async getAll() {
+    try {
+      const data = await fs.promises.readFile(this.filePath);
+      return JSON.parse(data);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async writeAll(data) {
+    try {
+      await fs.promises.writeFile(this.filePath, JSON.stringify(data));
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+class ProductFileManager extends FileManager {
+  async add(product) {
+    try {
+      const products = await this.getAll();
+      
+      product.id = products.length + 1;
+      
+      products.push(product);
+      
+      await this.writeAll(products);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async update(productId, updatedProduct) {
+    try {
+      const products = await this.getAll();
+
+      const index = products.findIndex((product) => product.id === productId);
+      if (index === -1) {
+        throw new Error("Producto no encontrado");
+      }
+
+      products[index] = { ...products[index], ...updatedProduct };
+
+      await this.writeAll(products);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async delete(productId) {
+    try {
+      const products = await this.getAll();
+
+      const index = products.findIndex((product) => product.id === productId);
+      if (index === -1) {
+        throw new Error("Producto no encontrado");
+      }
+
+      products.splice(index, 1);
+
+      await this.writeAll(products);
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+class CartFileManager extends FileManager {
+  async addProduct(cartId, productId) {
+    try {
+      const carts = await this.getAll();
+
+      const cart = carts.find((cart) => cart.id === cartId);
+      if (!cart) {
+        throw new Error("Carrito no encontrado");
+      }
+
+      cart.products.push(productId);
+
+      await this.writeAll(carts);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async deleteProduct(cartId, productId) {
+    try {
+      const carts = await this.getAll();
+
+      const cart = carts.find((cart) => cart.id === cartId);
+      if (!cart) {
+        throw new Error("Carrito no encontrado");
+      }
+
+      const index = cart.products.findIndex((product) => product === productId);
+      if (index === -1) {
+        throw new Error("Producto no encontrado");
+      }
+
+      cart.products.splice(index, 1);
+
+      await this.writeAll(carts);
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+export { ProductFileManager, CartFileManager, FileManager };
